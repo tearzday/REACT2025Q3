12ae01c5c117835e7ab3e63c5c42e89ab0613d6a
tearzday <biverdaniil@gmail.com>
2025-07-08 16:39:44 +0300

    feat: add card-list component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,11 @@
+import CardList from './components/card-list/CardList';
 import Search from './components/search/Search';
-import Button from './components/UI/button/Button';
-import Card from './components/UI/card/Card';
-import Input from './components/UI/input/Input';
 
 function App() {
-  const cardInfo = {
-    name: 'Card name',
-    description: 'Card description',
-    img: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
-  };
   return (
     <div className="app">
       <Search />
-
-      <Card info={cardInfo} />
+      <CardList />
     </div>
   );
 }
---
+++ src/components/card-list/CardList.tsx
@@ -0,0 +1,56 @@
+import { Component } from 'react';
+import Card from '../UI/card/Card';
+import classes from './CardList.module.scss';
+
+class CardList extends Component {
+  cardInfo = [
+    {
+      id: 0,
+      name: 'Card name',
+      description: 'Card description',
+      img: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+    },
+    {
+      id: 1,
+      name: 'Card name',
+      description: 'Card description',
+      img: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+    },
+    {
+      id: 2,
+      name: 'Card name',
+      description: 'Card description',
+      img: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+    },
+    {
+      id: 3,
+      name: 'Card name',
+      description: 'Card description',
+      img: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+    },
+    {
+      id: 4,
+      name: 'Card name',
+      description: 'Card description',
+      img: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+    },
+    {
+      id: 5,
+      name: 'Card name',
+      description: 'Card description',
+      img: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+    },
+  ];
+
+  render() {
+    return (
+      <div className={classes.card_list}>
+        {this.cardInfo.map((info) => (
+          <Card info={info} key={info.id} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default CardList;
