7015f6804cd8745e680e5e324afe9dcec194844d
tearzday <biverdaniil@gmail.com>
2025-07-09 18:50:06 +0300

    refactor: change ErrorBoundary content

--- src/components/error-boundary/ErrorBoundary.tsx
+++ src/components/error-boundary/ErrorBoundary.tsx
@@ -1,4 +1,6 @@
 import { Component, type ReactNode } from 'react';
+import Button from '../UI/button/Button';
+import styles from './ErrorBoundary.module.scss';
 
 interface ErrorBoundaryProps {
   children: ReactNode;
@@ -15,7 +17,12 @@ class ErrorBoundary extends Component<ErrorBoundaryProps> {
 
   render() {
     if (this.state.isError) {
-      return <div>Ошибка</div>;
+      return (
+        <div className={styles.error}>
+          <h2>Something went wrong behind the scenes.</h2>
+          <Button onClick={() => window.location.reload()}>Refresh page</Button>
+        </div>
+      );
     }
 
     return this.props.children;
