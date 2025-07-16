592661c24656da29688b65d4da27322d7a528f6e
tearzday <biverdaniil@gmail.com>
2025-07-16 18:00:17 +0300

    feat: add test for ErrorBoundary

---
+++ src/components/error-boundary/ErrorBoundary.test.tsx
@@ -0,0 +1,50 @@
+import { render, screen, waitFor } from '@testing-library/react';
+import ErrorBoundary from './ErrorBoundary';
+import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
+import Main from '../main/Main';
+
+const ProblemChildren = () => {
+  throw new Error('Child component error');
+};
+
+describe('Error Boundary Tests', () => {
+  test('Catches and handles JavaScript errors in child components and display fallback UI', () => {
+    render(
+      <ErrorBoundary>
+        <ProblemChildren />
+      </ErrorBoundary>
+    );
+
+    expect(screen.getByTestId('fallback')).toBeInTheDocument();
+  });
+
+  test('Logs error to console', () => {
+    const consoleError = vi
+      .spyOn(console, 'error')
+      .mockImplementation(() => {});
+
+    render(
+      <ErrorBoundary>
+        <ProblemChildren />
+      </ErrorBoundary>
+    );
+
+    expect(consoleError).toHaveBeenCalled();
+  });
+
+  test('Error Button Tests', async () => {
+    render(
+      <ErrorBoundary>
+        <Main cards={[]} loading={false} errorMessage="" />
+      </ErrorBoundary>
+    );
+
+    const btn = screen.getByText('Call Error');
+
+    userEvent.click(btn);
+    await waitFor(() =>
+      expect(screen.getByTestId('fallback')).toBeInTheDocument()
+    );
+  });
+});
