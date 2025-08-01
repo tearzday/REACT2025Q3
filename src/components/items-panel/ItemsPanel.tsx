e02c28af56bed4d36f0e9b6365d6557cfd5c3ea1
tearzday <biverdaniil@gmail.com>
2025-08-01 10:58:39 +0300

    feat: add download csv file

--- src/components/items-panel/ItemsPanel.tsx
+++ src/components/items-panel/ItemsPanel.tsx
@@ -1,11 +1,36 @@
 import useSelectedItems from '@/store/selectedItems';
 import Button from '../UI/button/Button';
 import style from './ItemsPanel.module.scss';
+import { useRef, useState } from 'react';
 
 function ItemsPanel() {
+  const [downloadUrl, setDownloadUrl] = useState('');
+  const downloadRef = useRef<HTMLAnchorElement | null>(null);
+
   const itemsCount = useSelectedItems((state) => state.itemsCount);
   const clear = useSelectedItems((state) => state.clear);
   const items = useSelectedItems((state) => state.items);
+
+  const handleDownload = () => {
+    const content = items.map(
+      (item, index) =>
+        `${index + 1}. ${item.name}, ${item.gender}, ${item.species}, ${item.status}\n`
+    );
+    const blob = new Blob(content, { type: 'text/csv;charset=utf-8' });
+
+    const url = URL.createObjectURL(blob);
+
+    setDownloadUrl(url);
+
+    setTimeout(() => {
+      const ref = downloadRef.current;
+      if (ref) {
+        ref.click();
+        URL.revokeObjectURL(url);
+        setDownloadUrl('');
+      }
+    }, 0);
+  };
 
   return (
     <div className={style.panel}>
@@ -14,14 +39,15 @@ function ItemsPanel() {
         <Button className={style.panel__btn} onClick={clear}>
           Unselect all
         </Button>
-        <Button
-          className={style.panel__btn}
-          onClick={() => {
-            console.log(items);
-          }}
-        >
+        <Button className={style.panel__btn} onClick={handleDownload}>
           Download
         </Button>
+        <a
+          ref={downloadRef}
+          href={downloadUrl}
+          className={style.blob__link}
+          download={`${itemsCount}_items.csv`}
+        />
       </div>
     </div>
   );
