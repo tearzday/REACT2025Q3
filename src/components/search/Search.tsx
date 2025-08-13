5ede499996b83f06f70c03889e79ab46e66976ff
tearzday <biverdaniil@gmail.com>
2025-08-13 16:20:31 +0300

    fix: local storage for search

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,14 +1,14 @@
 import Input from '../UI/input/Input';
 import Button from '../UI/button/Button';
 import style from './Search.module.scss';
-import { useState } from 'react';
-// import useLocalStorage from '@/hooks/useLocalStorage';
+import { useEffect, useState } from 'react';
+import useLocalStorage from '@/hooks/useLocalStorage';
 import useGetCards from '@/hooks/useGetCards';
 import useAppStore, { changeSearch, page, search } from '@/store/app';
 
 function Search() {
   const [value, setValue] = useState<string>('');
-  // const [lsValue, setLsValue] = useLocalStorage('search-character-value');
+  const [lsValue, setLsValue] = useLocalStorage('search-character-value');
 
   const currentPage = useAppStore(page);
   const currentSearch = useAppStore(search);
@@ -16,15 +16,15 @@ function Search() {
 
   useGetCards({ page: currentPage, name: currentSearch });
 
-  // useEffect(() => {
-  //   setValue(lsValue);
-  //   setCurrentSearch(lsValue);
-  // }, []);
+  useEffect(() => {
+    setValue(lsValue);
+    setCurrentSearch(lsValue);
+  }, []);
 
   const handlerClick = () => {
     const trimValue = value.trimEnd();
     setValue(trimValue);
-    // setLsValue(trimValue);
+    setLsValue(trimValue);
     setCurrentSearch(trimValue);
   };
 
