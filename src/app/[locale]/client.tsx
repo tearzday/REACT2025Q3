8a412f2b42565a6f165f2f70984f48090e297c26
tearzday <biverdaniil@gmail.com>
2025-08-14 20:57:24 +0300

    feat: add static fetch

--- src/app/[locale]/client.tsx
+++ src/app/[locale]/client.tsx
@@ -9,10 +9,24 @@ import Header from '@/components/header/Header';
 import { ThemeContext } from '@/context';
 import ItemsPanel from '@/components/items-panel/ItemsPanel';
 import useSelectedItems, { selectItemsCount } from '@/store/selectedItems';
+import { APIData } from '@/types';
+import useGetCards from '@/hooks/useGetCards';
+import useAppStore, { page, search } from '@/store/app';
 
-function HomeClient() {
+function HomeClient({ initialData }: { initialData: APIData }) {
+  const currentPage = useAppStore(page);
+  const currentSearch = useAppStore(search);
+
   const { theme } = useContext(ThemeContext);
   const itemsCount = useSelectedItems(selectItemsCount);
+  console.log(initialData);
+
+  useGetCards({
+    name: currentSearch,
+    page: currentPage,
+    initialData,
+  });
+
   return (
     <div className={theme}>
       <Header />
--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,5 +1,14 @@
+import { APIData } from '@/types';
 import HomeClient from './client';
+import APICard from '@/api/card';
 
-export default function Page() {
-  return <HomeClient />;
+export default async function Page() {
+  const body = {
+    name: '',
+    page: 1,
+  };
+
+  const data: APIData = await APICard.getCards(body);
+
+  return <HomeClient initialData={data} />;
 }
