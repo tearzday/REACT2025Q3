11b043383228634ce9098756fa32e3ca5dbaa0da
tearzday <biverdaniil@gmail.com>
2025-08-24 11:12:29 +0300

    refactor: close modal for esc and click overlay

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,4 +1,4 @@
-import type { ReactNode } from 'react';
+import { type ReactNode, useEffect } from 'react';
 
 interface ModalProps {
   onClose: () => void;
@@ -6,9 +6,29 @@ interface ModalProps {
 }
 
 export default function Modal({ onClose, children }: ModalProps) {
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (e.key === 'Escape') {
+        onClose();
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [onClose]);
+
   return (
-    <div className="w-svw h-svh bg-black/30 d-flex content-center justify-items-center absolute top-0">
-      <div className="w-3/6 bg-color-olive border-2 border-neutral-700 rounded-xl flex flex-col p-2 text-white">
+    <div
+      onClick={onClose}
+      className="w-svw h-svh bg-black/30 d-flex content-center justify-items-center absolute top-0"
+    >
+      <div
+        onClick={(e) => e.stopPropagation()}
+        className="w-3/6 bg-color-olive border-2 border-neutral-700 rounded-xl flex flex-col p-2 text-white"
+      >
         <button className="self-end" onClick={onClose}>
           Close
         </button>
