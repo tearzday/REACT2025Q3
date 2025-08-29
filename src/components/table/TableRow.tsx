574973ee590b8f178b2eb99c3bcd0ad9f7b7620b
tearzday <biverdaniil@gmail.com>
2025-08-29 16:52:29 +0300

    feat: add hightlight for change value

--- src/components/table/TableRow.tsx
+++ src/components/table/TableRow.tsx
@@ -1,5 +1,5 @@
 import type { CountryData } from '@/types';
-import { memo } from 'react';
+import { memo, useEffect, useRef, useState } from 'react';
 
 interface TableRowProps {
   countryName: string;
@@ -10,13 +10,43 @@ interface TableRowProps {
 
 const TableRow = memo(
   ({ countryName, iso_code, keys, data }: TableRowProps) => {
+    const [highlighted, setHighlighted] = useState<Record<string, boolean>>({});
+    const prevDataRef = useRef<CountryData | null>(null);
+
+    useEffect(() => {
+      const prevData = prevDataRef.current;
+
+      if (prevData) {
+        for (const key of keys) {
+          if (
+            prevData[key as keyof CountryData] !==
+              data[key as keyof CountryData] &&
+            key !== 'year'
+          ) {
+            setHighlighted((prev) => ({ ...prev, [key]: true }));
+
+            setTimeout(() => {
+              setHighlighted({});
+            }, 1000);
+          }
+        }
+      }
+
+      prevDataRef.current = data;
+    }, [data, keys]);
+
     return (
       <tr className="border border-slate-600">
         <td className="border border-slate-600 p-2">{countryName}</td>
         <td className="border border-slate-600 p-2">{iso_code}</td>
         {keys.map((key) => {
           return (
-            <td key={key} className="border border-slate-600 p-2">
+            <td
+              key={key}
+              className={`border border-slate-600 p-2 transition-colors duration-500 ${
+                highlighted[key] ? 'text-purple-400' : ''
+              }`}
+            >
               {data[key as keyof typeof data] ?? 'N/A'}
             </td>
           );
